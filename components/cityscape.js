9cefb0c11ade1a07aeb5fd50fa60ad027ecfeeb3
l4nk332 <l4nk332@gmail.com>
2016-09-19 11:47:20 -0700

    Begins constructing basic oop design

--- components/cityscape.js
+++ components/cityscape.js
@@ -1,14 +1,12 @@
 function init() {
 
 	var stats = initStats();
-	var scene = new THREE.Scene();
 
 	var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-	scene.add(camera);
 
 	var renderer = new THREE.WebGLRenderer();
 
-	renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+	renderer.setClearColor(new THREE.Color(0x333333, 1.0));
 	renderer.setSize(window.innerWidth, window.innerHeight);
 
 	document.getElementById("WebGL-output").appendChild(renderer.domElement);
@@ -16,16 +14,40 @@ function init() {
 	camera.position.x = -30;
 	camera.position.y = 40;
 	camera.position.z = 30;
-	camera.lookAt(scene.position);
 
 	var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-	scene.add(ambientLight);
 
 	// Begin Here....
+	var City = function(size, buroughs) {
+	    this.scene = new THREE.Scene();
+	    this.size = size;
+	    this.step = 1;
+	    this.grid = new THREE.GridHelper(this.size, this.step);
+	    this.scene.add(this.grid);
+	    this.buroughs = buroughs;
+	}
+
+	var Building = function(type, geometry, material) {
+	    this.type = type;
+	    this.geometry = geometry;
+	    // Moves pivot point to bottom
+	    this.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, (this.geometry.parameters.height)/2, 0));
+	    // Remove bottom face
+	    this.geometry.faces.splice(6, 1);
+	    this.material = material;
+	    this.mesh = new THREE.Mesh(this.geometry, this.material);
+	}
+
+	var cityscape = new City(100, 5);
+
 	var geo = new THREE.BoxGeometry(10, 10, 10);
-	var mat = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-	var mesh = new THREE.Mesh(geo, mat);
-	scene.add(mesh);
+	var mat = new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: false});
+	var buildingBasic = new Building("basicBox", geo, mat);
+
+	cityscape.scene.add(buildingBasic.mesh);
+	cityscape.scene.add(camera);
+	camera.lookAt(cityscape.scene.position);
+	cityscape.scene.add(ambientLight);
 
 	// var controls = new function () {
 	// 	// control code here...
@@ -41,7 +63,7 @@ function init() {
 
 		// render using requestAnimationFrame
 		requestAnimationFrame(render);
-		renderer.render(scene, camera);
+		renderer.render(cityscape.scene, camera);
 	}
 
 	function initStats() {
